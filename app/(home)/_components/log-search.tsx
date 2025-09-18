36f4fb4cf8e8aca090ffd804ce3ac8c010e29384
Chiel <chielbleyenbergh@gmail.com>
2025-09-18 18:32:21 +0200

    better placeholder text on search log

--- app/(home)/_components/log-search.tsx
+++ app/(home)/_components/log-search.tsx
@@ -9,7 +9,7 @@ export default function LogSearch({
   return (
     <input
       type="text"
-      placeholder="Search your log ..."
+      placeholder="Search your log on, author id, message and source ..."
       value={searchValue}
       onChange={(e) => onSearch(e.target.value)}
       className="w-full max-w-xl focus:outline-primary-500 border border-primary-400 rounded-full px-4 py-2 m-auto transition-transform duration-300 lg:focus:scale-105"
