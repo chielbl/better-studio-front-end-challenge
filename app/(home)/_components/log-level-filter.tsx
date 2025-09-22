e3d60df75c020c74ead137c9ef7cd4fbe9624bae
Chiel <chielbleyenbergh@gmail.com>
2025-09-22 23:25:43 +0200

    Remove console.log

--- app/(home)/_components/log-level-filter.tsx
+++ app/(home)/_components/log-level-filter.tsx
@@ -10,20 +10,17 @@ export default function LogLevelFilter({
 }: LogLevelFilterProps) {
   return (
     <div className="flex border border-solid border-primary-400 rounded-full overflow-hidden">
-      {levels.map((level) => {
-        console.log(level === selectedLevel);
-        return (
-          <button
-            key={level}
-            onClick={() => levelOnClick(level)}
-            className={`px-2 py-1 overflow-hidden cursor-pointer hover:bg-primary-400 hover:text-primary-50 transition-colors duration-300 ${
-              level === selectedLevel && "bg-primary-600 text-primary-50"
-            }`}
-          >
-            {level}
-          </button>
-        );
-      })}
+      {levels.map((level) => (
+        <button
+          key={level}
+          onClick={() => levelOnClick(level)}
+          className={`px-2 py-1 overflow-hidden cursor-pointer hover:bg-primary-400 hover:text-primary-50 transition-colors duration-300 ${
+            level === selectedLevel && "bg-primary-600 text-primary-50"
+          }`}
+        >
+          {level}
+        </button>
+      ))}
     </div>
   );
 }
